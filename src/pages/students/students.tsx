ccd193b122a09419716e87ccd636fb7b28773f19
abdullox0900 <abdullox0900@gmail.com>
2024-04-08 15:19:13 +0500

    edit design teacher list

--- src/pages/students/students.tsx
+++ src/pages/students/students.tsx
@@ -20,6 +20,10 @@ export default function Students() {
         setTitle('Students List')
     }, [])
 
+    useEffect(() => {
+        // axios('')
+    }, [])
+
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     return (
--- src/pages/teachers/teachers.tsx
+++ src/pages/teachers/teachers.tsx
@@ -1,10 +1,10 @@
 import { Input, Select } from 'antd'
 import { useEffect, useState } from 'react'
 import { IoIosAdd } from 'react-icons/io'
-import Main from '../../components/main/main'
-import useTitle from '../../hooks/useTitle'
 import Modal from 'react-responsive-modal'
 import TeacherForm from '../../components/forms/teacher_form/teacher_form'
+import Main from '../../components/main/main'
+import useTitle from '../../hooks/useTitle'
 
 export default function Teachers() {
 
@@ -45,33 +45,36 @@ export default function Teachers() {
                     <IoIosAdd className='text-[#ffff]' />
                 </button>
             </div>
-            <div className='flex justify-between w-[100%] rounded-[10px] p-[18px] border border-[rgba(0,0,0,0.16)_0px_1px_4px] mb-[25px]'>
-                <span className='w-[50px] font-semibold text-[#4D525B] text-[14px]'>Rs</span>
-                <span className='w-[200px] font-semibold text-[#4D525B] text-[14px]'>F.I.O</span>
-                <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Yonalish</span>
-                <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Guruhlari</span>
-                <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Telfon raqam</span>
-                <span className='w-[150px] font-semibold text-[#4D525B] text-[14px] text-right'>Amallar</span>
-            </div>
-            <ul className='flex flex-col gap-[15px]'>
-                {
-                    arr.map((item) => {
-                        return (
-                            <li className='flex items-center justify-between w-[100%] rounded-[10px] px-[18px] py-[10px] border border-[rgba(0,0,0,0.16)_0px_1px_4px] hover:shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] cursor-pointer transition-all ease' key={item}>
-                                <div className='w-[50px] h-[31px]'>
-                                    <span className='w-[30px] h-[30px] bg-slate-400 rounded-full'></span>
-                                </div>
-                                <span className='w-[200px] text-[14px] text-slate-400'>Abdulloh Abdusalomov</span>
-                                <span className='w-[150px] text-[14px] text-slate-400'>Frontend</span>
-                                <span className='w-[150px] text-[14px] text-slate-400'>4</span>
-                                <span className='w-[150px] text-[14px] text-slate-400'>90-043-11-10</span>
-                                <span className='w-[150px] text-[14px] text-slate-400 text-right'></span>
-                            </li>
-                        )
-                    })
-                }
+            <div className="max-h-[73vh] sticky overflow-y-auto rounded-[10px] border border-[rgba(0,0,0,0.16)_0px_1px_4px]">
+                <div className='flex sticky top-0 justify-between w-[100%] p-[18px] bg-white mb-[25px] border-b border-[rgba(0,0,0,0.16)_0px_1px_4px]'>
+                    <span className='w-[50px] font-semibold text-[#4D525B] text-[14px]'>Rs</span>
+                    <span className='w-[200px] font-semibold text-[#4D525B] text-[14px]'>F.I.O</span>
+                    <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Yonalish</span>
+                    <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Guruhlari</span>
+                    <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Telfon raqam</span>
+                    <span className='w-[150px] font-semibold text-[#4D525B] text-[14px] text-right'>Amallar</span>
+                </div>
+                <ul className='flex flex-col '>
+                    {
+                        arr.map((item) => {
+                            return (
+                                <li className='flex items-center justify-between w-[100%] px-[18px] py-[20px] border-b border-[rgba(0,0,0,0.16)_0px_1px_4px] hover:shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] cursor-pointer transition-all ease' key={item}>
+                                    <div className='w-[50px] h-[31px]'>
+                                        <span className='w-[30px] h-[30px] bg-slate-400 rounded-full'></span>
+                                    </div>
+                                    <span className='w-[200px] text-[14px] text-slate-400'>Abdulloh Abdusalomov</span>
+                                    <span className='w-[150px] text-[14px] text-slate-400'>Frontend</span>
+                                    <span className='w-[150px] text-[14px] text-slate-400'>4</span>
+                                    <span className='w-[150px] text-[14px] text-slate-400'>90-043-11-10</span>
+                                    <span className='w-[150px] text-[14px] text-slate-400 text-right'></span>
+                                </li>
+                            )
+                        })
+                    }
 
-            </ul>
+                </ul>
+            </div>
+
             <Modal open={open} onClose={onCloseModal} center
                 classNames={{
                     overlay: 'customOverlay',
