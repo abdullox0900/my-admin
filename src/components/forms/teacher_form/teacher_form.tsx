1106b00e78adb16b172e3269c5e017bc2b0056ac
abdullox0900 <abdullox0900@gmail.com>
2024-04-16 13:14:51 +0500

    add teacher form finish

--- src/components/forms/teacher_form/teacher_form.tsx
+++ src/components/forms/teacher_form/teacher_form.tsx
@@ -1,22 +1,104 @@
-import { Button, Input, Select, DatePicker } from "antd"
+import { Button, DatePicker, Input, Radio, Select, message } from "antd"
+import axios from 'axios'
+import { useState } from 'react'
 
 function TeacherForm() {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [birthDate, setBirthDate] = useState('')
+    const [phone, setPhone] = useState('')
+    const [genre, setGenre] = useState('')
+    const [status, setStatus] = useState('')
+
+    const [messageApi, contextHolder] = message.useMessage()
+
+    const success = () => {
+        messageApi.open({
+            type: 'success',
+            content: 'Ma’lumot  jo‘natildi 😊',
+        })
+    }
+
+    const error = () => {
+        messageApi.open({
+            type: 'error',
+            content: 'Xatolik yuz berdi! 😱',
+        })
+    }
+
+    const warning = () => {
+        messageApi.open({
+            type: 'warning',
+            content: 'Ma’lumot toldiring 😕',
+        })
+    }
+
+    const clearInput = () => {
+        setFirstName('')
+        setLastName('')
+        setBirthDate('')
+        setGenre('')
+        setPhone('')
+        setStatus('')
+    }
+
+    const onSubmit = () => {
+        if (firstName !== '' && lastName !== '' && birthDate !== '' && genre !== '' && status !== '' && phone !== '') {
+            const data = {
+                first_name: firstName,
+                last_name: lastName,
+                birth_date: birthDate,
+                gender: genre,
+                status: status,
+                phone: phone,
+            }
+
+            try {
+                axios.post('https://edu.backofficee.uz/api/v1/teachers/', data).then(res => {
+                    if (res) {
+                        clearInput()
+                    }
+                }).finally(() => {
+                    success()
+                }).catch(() => {
+                    error()
+                })
+            } catch (err) {
+                error()
+            }
+        } else {
+            warning()
+
+        }
+    }
+
     return (
-        <form className='flex flex-col gap-[20px]'>
-            <Input placeholder='Ism' size='large' />
-            <Input placeholder='Telfon raqami' size='large' />
-            <Input placeholder='Login' size='large' />
-            <DatePicker size='large' />
-            <Select
-                placeholder="Yonalish"
-                size='large'
-                options={[
-                    { value: 'jack', label: 'Frontend' },
-                    { value: 'lucy', label: 'Backend' }
-                ]}
-            />
-            <Button size='large'>Yaratish</Button>
-        </form>
+        <>
+            {contextHolder}
+            <form className='flex flex-col gap-[20px]'>
+                <Input placeholder='Ism' value={firstName} size='large' onChange={(e) => setFirstName(e.target.value)} />
+                <Input placeholder='Falimiya' value={lastName} size='large' onChange={(e) => setLastName(e.target.value)} />
+                <Input placeholder='Telfon raqam' value={phone} size='large' onChange={(e) => setPhone(e.target.value)} />
+                <DatePicker placeholder='Tugilgan kuni' size='large' onChange={(e) => setBirthDate(e.format().slice(0, 10))
+                } />
+                <Select
+                    placeholder="Yonalish"
+                    value={status}
+                    size='large'
+                    onChange={(value) => setStatus(value)}
+                    options={[
+                        { value: '1', label: 'Frontend' },
+                        { value: '2', label: 'Backend' }
+                    ]}
+                />
+                <Radio.Group value={genre} onChange={(e) => setGenre(e.target.value)}>
+                    <Radio value={'male'}>Male</Radio>
+                    <Radio value={'female'}>Female</Radio>
+                </Radio.Group>
+                <Button size='large' onClick={onSubmit}>Yaratish</Button>
+            </form>
+        </>
     )
 }
 
--- src/pages/teachers/teachers.tsx
+++ src/pages/teachers/teachers.tsx
@@ -1,10 +1,26 @@
-import { Input, Select } from 'antd'
+import { useQuery } from '@tanstack/react-query'
+import { Empty, Input, Select, Spin } from 'antd'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { IoIosAdd } from 'react-icons/io'
 import Modal from 'react-responsive-modal'
 import TeacherForm from '../../components/forms/teacher_form/teacher_form'
 import Main from '../../components/main/main'
 import useTitle from '../../hooks/useTitle'
+
+interface DataType {
+    id: number
+    first_name: string
+    last_name: string
+    age: number
+    status: number
+    birth_date: string
+    updated_at: string
+    created_at: string
+    deleted_at: string
+    gender: string
+    phone: string
+}
 
 export default function Teachers() {
 
@@ -16,10 +32,24 @@ export default function Teachers() {
 
     useEffect(() => {
         setTitle('Teachers list')
-
     }, [])
 
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // https://edu.backofficee.uz/api/v1/teachers/
+
+    const teachersData = async () => {
+        const response = await axios.get('https://edu.backofficee.uz/api/v1/teachers/')
+        return response.data
+    }
+
+    const { isLoading, isError, data } = useQuery({
+        queryKey: ['teacher'],
+        queryFn: teachersData
+    })
+
+    // ======= // Is Error // ======= //
+    if (isError) {
+        return <h1>Malumot olishda xatoli yuzberdi</h1>
+    }
 
     return (
         <Main>
@@ -45,8 +75,8 @@ export default function Teachers() {
                     <IoIosAdd className='text-[#ffff]' />
                 </button>
             </div>
-            <div className="max-h-[73vh] sticky overflow-y-auto rounded-[10px] border border-[rgba(0,0,0,0.16)_0px_1px_4px]">
-                <div className='flex sticky top-0 justify-between w-[100%] p-[18px] bg-white mb-[25px] border-b border-[rgba(0,0,0,0.16)_0px_1px_4px]'>
+            <div className="max-h-[73vh] sticky overflow-y-auto">
+                <div className='flex sticky top-0 justify-between w-[100%] p-[18px] bg-white mb-[5px]'>
                     <span className='w-[50px] font-semibold text-[#4D525B] text-[14px]'>Rs</span>
                     <span className='w-[200px] font-semibold text-[#4D525B] text-[14px]'>F.I.O</span>
                     <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Yonalish</span>
@@ -54,25 +84,34 @@ export default function Teachers() {
                     <span className='w-[150px] font-semibold text-[#4D525B] text-[14px]'>Telfon raqam</span>
                     <span className='w-[150px] font-semibold text-[#4D525B] text-[14px] text-right'>Amallar</span>
                 </div>
-                <ul className='flex flex-col '>
-                    {
-                        arr.map((item) => {
-                            return (
-                                <li className='flex items-center justify-between w-[100%] px-[18px] py-[20px] border-b border-[rgba(0,0,0,0.16)_0px_1px_4px] hover:shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] cursor-pointer transition-all ease' key={item}>
-                                    <div className='w-[50px] h-[31px]'>
-                                        <span className='w-[30px] h-[30px] bg-slate-400 rounded-full'></span>
-                                    </div>
-                                    <span className='w-[200px] text-[14px] text-slate-400'>Abdulloh Abdusalomov</span>
-                                    <span className='w-[150px] text-[14px] text-slate-400'>Frontend</span>
-                                    <span className='w-[150px] text-[14px] text-slate-400'>4</span>
-                                    <span className='w-[150px] text-[14px] text-slate-400'>90-043-11-10</span>
-                                    <span className='w-[150px] text-[14px] text-slate-400 text-right'></span>
-                                </li>
-                            )
-                        })
-                    }
-
-                </ul>
+                {
+                    data?.length == 0 ? (
+                        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                    ) : (
+                        <ul className='flex flex-col gap-[15px]'>
+                            {
+                                isLoading ? (
+                                    <Spin />
+                                ) : (
+                                    data?.slice(0, 8)?.map((item: DataType) => {
+                                        return (
+                                            <li className='flex items-center justify-between w-[100%] px-[18px] py-[15px] bg-white border border-gray-200  rounded-[8px] cursor-pointer transition-all ease hover:bg-gray-200' key={item.id}>
+                                                <div className='w-[50px] h-[31px]'>
+                                                    <span className=' flex justify-center items-center w-[30px] h-[30px] bg-orange-400 rounded-full text-white'>{item.first_name.charAt(0)}</span>
+                                                </div>
+                                                <span className='w-[200px] text-[16px] text-slate-500 font-semibold'>{`${item.first_name} ${item.last_name}`}</span>
+                                                <span className='w-[150px] text-[16px] text-slate-500'>{item.status}</span>
+                                                <span className='w-[150px] text-[16px] text-slate-500'>0</span>
+                                                <span className='w-[150px] text-[16px] text-slate-500'>{item.phone}</span>
+                                                <span className='w-[150px] text-[14px] text-slate-400 text-right'></span>
+                                            </li>
+                                        )
+                                    })
+                                )
+                            }
+                        </ul>
+                    )
+                }
             </div>
 
             <Modal open={open} onClose={onCloseModal} center
@@ -83,6 +122,6 @@ export default function Teachers() {
                 <h2 className='text-center mb-[25px] text-[22px] text-gray-600 font-medium'>Teacher qo‘shish</h2>
                 <TeacherForm />
             </Modal>
-        </Main>
+        </Main >
     )
 }
